6af8672d939cbb5c343053ffb2d4b1a89362265e
adityaakhileshwaran <aakhiles@cs.washinton.edu>
2024-06-17 00:13:35 -0700

    Small change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
 
 
